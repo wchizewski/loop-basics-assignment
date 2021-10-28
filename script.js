a67e5745793bcaa62823ad8e095040b77a8a8ad1
wchizewski <w.chizewski@share.epsb.ca>
2021-10-28 13:39:30 -0600

    thrudyas

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 // “I’m so happy!” 500 times
-// for (let num = 1; num <= 500; num++) {
-//     console.log("I'm so happy!")
-// }
+for (let num = 1; num <= 500; num++) {
+    console.log("I'm so happy!")
+}
+console.log("--------------------------------------")
 
 // all of the multiples of 4 from 12 to 800
-// for (let num = 12; num <= 800; num += 4) {
-//     console.log(num)
-// }
+for (let num = 12; num <= 800; num += 4) {
+    console.log(num)
+}
+console.log("--------------------------------------")
+
 
 // all of the odd numbers from 55 to 11
-// for (let num = 55; num >= 11; num -= 2) {
-//     console.log(num)
-// }
+for (let num = 55; num >= 11; num -= 2) {
+    console.log(num)
+}
+console.log("--------------------------------------")
+
 
 // sum of the series: 5 + 6 + 7 + 8 + … + 48 + 49 + 50
-// let sum = 0;
-// for (let num = 5; num <= 50; num++) {
-//     sum = sum + num;
-// }
-// console.log(sum)
+let sum = 0;
+for (let num = 5; num <= 50; num++) {
+    sum = sum + num;
+}
+console.log(sum)
+console.log("--------------------------------------")
+
 
 // sum of the series: 10 + 20 + 30 + … + 90 + 100
-// let sum = 0;
-// for (let num = 10; num <= 100; num += 10){
-//     sum = sum + num;
-// }
-// console.log(sum)
+sum = 0;
+for (let num = 10; num <= 100; num += 10){
+    sum = sum + num;
+}
+console.log(sum)
